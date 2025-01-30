6612205e4dd77250710a440a660ee47e2fda48c0
YhyCoder <192763896+YhyCoder@users.noreply.github.com>
2025-01-30 17:25:01 +0330

    Create playRound function & test it work correct

--- script.js
+++ script.js
@@ -27,8 +27,56 @@ function getHumanChoice(userChoice) {
       return "Scissors";
     default:
       alert("Please enter a valid move");
+      return userChoice;
   }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice(userChoice));
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "Rock") {
+    switch (computerChoice) {
+      case "Scissors":
+        console.log("Congratulations, You win! Rock beats Scissors");
+        humanScore++;
+        break;
+      case "Paper":
+        console.log("You lose! Paper beats Rock");
+        computerScore++;
+        break;
+      default:
+        console.log("Draw!");
+    }
+  } else if (humanChoice === "Paper") {
+    switch (computerChoice) {
+      case "Rock":
+        console.log("Congratulations, You win! Paper beats Rock");
+        humanScore++;
+        break;
+      case "Scissors":
+        console.log("You lose! Scissors beats Paper");
+        computerScore++;
+        break;
+      default:
+        console.log("Draw!");
+    }
+  } else if (humanChoice === "Scissors") {
+    switch (computerChoice) {
+      case "Paper":
+        console.log("Congratulations, You win! Scissors beats Paper");
+        humanScore++;
+        break;
+      case "Rock":
+        console.log("You lose! Rock beats Scissors");
+        computerScore++;
+        break;
+      default:
+        console.log("Draw!");
+    }
+  } else {
+    console.log(`${humanChoice} is not valid choice, Please use Rock, Paper or Scissors!`);
+  }
+}
+
+const humanSelection = getHumanChoice(userChoice);
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
